15c54d199746b64789bab1b381b3a281c7eb4192
Park-Seung-Hun <psh19930326@gmail.com>
2021-05-02 19:08:42 +0900

    #3.0 Class Components and State

--- src/App.js
+++ src/App.js
@@ -1,74 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1,
-    name: "Kimchi",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-    rating: 1.3,
-  },
-  {
-    id: 2,
-    name: "Samgyeopsal",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-    rating: 4.9,
-  },
-  {
-    id: 3,
-    name: "Bibimbap",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-    rating: 4.8,
-  },
-  {
-    id: 4,
-    name: "Doncasu",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-    rating: 5.5,
-  },
-  {
-    id: 5,
-    name: "Kimbap",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-    rating: 4.7,
-  },
-];
+class App extends React.Component {
+  state = {
+    count: 0,
+  };
 
-function Food({ name, pic, rating }) {
-  return (
-    <div>
-      <h2>나의 최애 음식은 {name}입니다.</h2>
-      <h4>{rating}/5.0</h4>
-      <img src={pic} alt={name} />
-    </div>
-  );
-}
-
-Food.propTypes = {
-  // 내가 얻고 싶은 props에 대한 설명을 넣는다.
-  name: PropTypes.string.isRequired,
-  pic: PropTypes.string.isRequired,
-  rating: PropTypes.number.isRequired,
-};
-
-function App() {
-  return (
-    <div>
-      {foodILike.map((dish) => (
-        <Food
-          key={dish.id}
-          name={dish.name}
-          pic={dish.image}
-          rating={dish.rating}
-        />
-      ))}
-    </div>
-  );
+  add = () => {
+    console.log("증가");
+  };
+  minus = () => {
+    console.log("감소");
+  };
+  render() {
+    return (
+      <div>
+        <h1>지금 나오는 숫자는 {this.state.count}</h1>
+        <button onClick={this.add}>증가</button>
+        <button onClick={this.minus}>감소</button>
+      </div>
+    );
+  }
 }
 
 export default App;
