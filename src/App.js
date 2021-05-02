16398cf0c9148d60d0d92fd4b912f54fa657aa24
Park-Seung-Hun <psh19930326@gmail.com>
2021-05-02 13:02:04 +0900

    #2.1 Reusable Components with JSX + Props

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
-import Posta from "./posta";
+
+function Food(props) {
+  console.log(props, props.example1, props.example2, props.example3); // props는 객체로써 전달된다.
+  return <h2>나는 {props.example1}입니다.</h2>;
+}
+/*
+단일 props의 사용법
+function Food({fav}) {
+  return <h2>나는 {fav}입니다.</h2>;
+}
+
+<Food fav="김치" />
+*/
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
-      <Posta />
+      <Food example1="감자" example2={true} example3={[1, 2, 3, 4, "hello"]} />
     </div>
   );
 }
--- src/posta.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Posta() {
-  return <div>내 이름은 Phodol2</div>;
-}
-
-export default Posta;
