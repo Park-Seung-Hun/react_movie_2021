2fa6600310ff145a97860ece2027df235f01e9b6
Park-Seung-Hun <psh19930326@gmail.com>
2021-05-02 14:25:50 +0900

    #2.4 Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,52 +1,71 @@
 import React from "react";
+import PropTypes from "prop-types";
 
-function Food({ name, pic }) {
+const foodILike = [
+  {
+    id: 1,
+    name: "Kimchi",
+    image:
+      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
+    rating: 1.3,
+  },
+  {
+    id: 2,
+    name: "Samgyeopsal",
+    image:
+      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
+    rating: 4.9,
+  },
+  {
+    id: 3,
+    name: "Bibimbap",
+    image:
+      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
+    rating: 4.8,
+  },
+  {
+    id: 4,
+    name: "Doncasu",
+    image:
+      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
+    rating: 5.5,
+  },
+  {
+    id: 5,
+    name: "Kimbap",
+    image:
+      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
+    rating: 4.7,
+  },
+];
+
+function Food({ name, pic, rating }) {
   return (
     <div>
       <h2>나의 최애 음식은 {name}입니다.</h2>
+      <h4>{rating}/5.0</h4>
       <img src={pic} alt={name} />
     </div>
   );
 }
 
-const foodILike = [
-  {
-    id: 1,
-    name: "김치",
-    image:
-      "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg",
-  },
-  {
-    id: 2,
-    name: "삼겹살",
-    image:
-      "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg",
-  },
-  {
-    id: 3,
-    name: "비빔밥",
-    image:
-      "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb",
-  },
-  {
-    id: 4,
-    name: "돈까스",
-    image:
-      "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg",
-  },
-  {
-    id: 5,
-    name: "김밥",
-    image:
-      "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg",
-  },
-];
+Food.propTypes = {
+  // 내가 얻고 싶은 props에 대한 설명을 넣는다.
+  name: PropTypes.string.isRequired,
+  pic: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired,
+};
 
 function App() {
   return (
     <div>
       {foodILike.map((dish) => (
-        <Food key={dish.id} name={dish.name} pic={dish.image} />
+        <Food
+          key={dish.id}
+          name={dish.name}
+          pic={dish.image}
+          rating={dish.rating}
+        />
       ))}
     </div>
   );
