37bec09fbf751d87221d8369d0b0c387e7ca3c9e
Park-Seung-Hun <psh19930326@gmail.com>
2021-05-02 12:06:26 +0900

    #2.0 Creating your first React Component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
+import Posta from "./posta";
 
 function App() {
   return (
     <div>
       <h1>Hello!!!</h1>
+      <Posta />
     </div>
   );
 }
---
+++ src/posta.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Posta() {
+  return <div>내 이름은 Phodol2</div>;
+}
+
+export default Posta;
