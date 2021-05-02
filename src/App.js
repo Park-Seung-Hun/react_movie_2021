a18f45ecdc840e4edd62954ae3da1660c2d4a3d1
Park-Seung-Hun <psh19930326@gmail.com>
2021-05-02 22:11:28 +0900

    #4.0 Fetching Movies from API

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from "react";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
     isLoading: true,
     movies: [],
   };
+
+  getMovies = async () => {
+    // Axios에서 온  Data를 잡는 과정
+    // Axios는 느린 과정이기에 비동기처리
+    const movies = await axios.get(
+      "https://yts-proxy.nomadcoders1.now.sh/list_movies.json"
+    );
+  };
   componentDidMount() {
-    setTimeout(() => {
-      // JS함수 이다.
-      this.setState({ isLoading: false });
-    }, 6000);
-  } // 1분 로딩창 설계
+    this.getMovies(); // 비동기 처리를 위해 함수 생성.
+  }
   render() {
     const { isLoading } = this.state;
     return <div>{isLoading ? "로딩중...😪" : "준비 완료!😁"}</div>;
