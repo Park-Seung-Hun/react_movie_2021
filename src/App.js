8b4e4e36a92789fda77478b77e56f61365835c40
Park-Seung-Hun <psh19930326@gmail.com>
2021-05-02 19:53:09 +0900

    #3.2 Component Life Cycle

--- src/App.js
+++ src/App.js
@@ -11,7 +11,17 @@ class App extends React.Component {
   minus = () => {
     this.setState((current) => ({ count: current.count - 1 }));
   };
+  componentDidMount() {
+    console.log("마운트"); // mount 동작 (새로고침 등)
+  }
+  componentDidUpdate() {
+    console.log("갱신"); // update 동작
+  }
+  componentWillUnmount() {
+    console.log("언마운트 "); // 변경사항을 저장하면 확인 가능
+  }
   render() {
+    console.log("렌더링"); // render 동작 (새로고침 등)
     return (
       <div>
         <h1>지금 나오는 숫자는 {this.state.count}</h1>
