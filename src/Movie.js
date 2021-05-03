010b8e2b01ec89683f6e26bb1c637f2de04d5bdc
Park-Seung-Hun <psh19930326@gmail.com>
2021-05-03 17:52:51 +0900

    #4.2 Styling the Movies

--- src/Movie.js
+++ src/Movie.js
@@ -1,10 +1,20 @@
 /* 실제로 movies를 render하기 위한 목적 */
 import React from "react";
 import PropTypes from "prop-types";
+import "./Movie.css";
 
 /* 함수형 컴포넌트 사용 -> props를 위해서 */
 function Movie({ id, year, title, summary, poster }) {
-  return <h4>{title}</h4>;
+  return (
+    <div class="movie">
+      <img src={poster} alt={title} title={title}></img>
+      <div class="movie_data">
+        <h3 class="movie_title">{title}</h3>
+        <h5 class="movie_year">{year}</h5>
+        <p class="movie_summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
 
 /* propTypes를 설정해 prop의 타입을 정한다. */
