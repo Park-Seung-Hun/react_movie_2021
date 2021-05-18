d71c7450b25a9953f3757413346cdaf55404bf6b
Park-Seung-Hun <psh19930326@gmail.com>
2021-05-18 10:23:26 +0900

    feat: add hashrouter and route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,23 @@
 import React from "react";
+import { HashRouter, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+
 function App() {
-  return <span>something</span>;
+  return (
+    // #주소를 이용해 route 구분
+    <HashRouter>
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+    </HashRouter>
+  );
 }
+
+/*
+  <Route>
+    path => url에 붙는 주소 값을 나타낸다
+    component => 해당 url일때 나타나는 component
+    exact={true} => 해당 path일 때만 rendering이 되게끔 설정
+*/
 
 export default App;
